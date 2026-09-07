a8f5e05e4081dfe282c11a13e4b8f8d806b44274
Ggoro Yattemiyo <goro.money.business@gmail.com>
2026-09-07 21:22:35 +0900

    style: load recorder UI styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './mobile-overrides.css'
+import './recorder.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
