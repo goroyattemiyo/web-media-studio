12ae98574b687a66f70b38feefd5502e352e639e
Ggoro Yattemiyo <goro.money.business@gmail.com>
2026-09-08 18:32:06 +0900

    refactor: remove legacy Cloud Run localizer UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import YouTubeProviderPanel from './YouTubeProviderPanel'
-import YouTubeLocalizerPanel from './YouTubeLocalizerPanel'
-import ColabLocalizerCompanion from './ColabLocalizerCompanion'
 import ToolDeckEnhancer from './ToolDeckEnhancer'
 import { installPlaybackArbitration } from './playbackArbiter'
 import './styles.css'
@@ -17,8 +15,6 @@ import './player-visuals.css'
 import './reorder-resume.css'
 import './named-playlists.css'
 import './youtube-provider.css'
-import './youtube-localizer.css'
-import './colab-localizer.css'
 import './ui-v2.css'
 
 installPlaybackArbitration()
@@ -35,9 +31,7 @@ function Root() {
   return (
     <>
       <App key={libraryRevision} />
-      <YouTubeProviderPanel />
-      <YouTubeLocalizerPanel onMediaLocalized={() => setLibraryRevision((value) => value + 1)} />
-      <ColabLocalizerCompanion />
+      <YouTubeProviderPanel onMediaImported={() => setLibraryRevision((value) => value + 1)} />
       <ToolDeckEnhancer />
     </>
   )
