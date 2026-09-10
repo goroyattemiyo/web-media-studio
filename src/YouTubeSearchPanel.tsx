4092e11eebcad98006b01632a1dffe4d9306f445
Ggoro Yattemiyo <goro.money.business@gmail.com>
2026-09-10 10:52:53 +0900

    chore: use type-only React import

--- src/YouTubeSearchPanel.tsx
+++ src/YouTubeSearchPanel.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import type { FormEvent } from 'react'
 import { createPortal } from 'react-dom'
 import { requestLoadYouTubeSource } from './mediaSourceBridge'
 import { addYouTubeToPlayQueue } from './playQueueBridge'
